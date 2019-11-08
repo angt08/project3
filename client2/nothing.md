517cbb8540f78e25d30d5c8630d427eb1b57d5b5
Ben Grzybowski <bengrzybowski@Bens-MacBook-Pro.local>
2019-11-08 12:08:19 -0500

    updated db

---
+++ client2/nothing.md
@@ -0,0 +1,5 @@
+this is junk
+
+dnsjdba
+dnjsbdja
+ndjsabdj
---
+++ client2/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+export default function Header(props) {
+
+  return (
+    <header>
+      <h1>Application Name</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/about">About</Link>
+      </nav>
+    </header>
+  )
+}
+
