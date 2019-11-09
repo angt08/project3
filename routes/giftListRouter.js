72502cc5aeba4c010003e3c7c9e68b7dbf00a71a
Ben Grzybowski <bengrzybowski@bens-mbp.lan>
2019-11-08 23:32:17 -0500

    fixed backend issues

--- routes/giftListRouter.js
+++ routes/giftListRouter.js
@@ -6,12 +6,7 @@ const giftListRouter = Router();
 
 // route for all giftlists user owns
 giftListRouter.get("/", async (req, res) => {
-  const userId = req.params.userId
-  const giftlists = await GiftList.findAll({
-    where: {
-      user_id: userId
-    }
-  });
+  const giftlists = await GiftList.findAll();
   res.json({ giftlists })
 })
 
@@ -19,14 +14,19 @@ giftListRouter.get("/", async (req, res) => {
 // get giftlist  fin by pk
 giftListRouter.get("/:id", async (req, res) => {
   const id = req.params.id
-  const giftslist = await GiftList.findByPk(id);
-  res.json({ giftlist })
+  const giftlists = await GiftList.findAll({
+    where: {
+      user_id: id
+    }
+  });
+  res.json({ giftlists })
 })
 
 // create new giftlist 
-giftListRouter.post("/", async (req, res) => {
+giftListRouter.post("/:id", async (req, res) => {
+  console.log("here")
   const data = req.body
-  const userId = req.params.userId
+  const userId = req.params.id
   const user = await User.findByPk(userId)
   const giftlist = await GiftList.create(data)
   giftlist.setUser(user)
@@ -39,12 +39,11 @@ giftListRouter.put("/:id", async (req, res) => {
   const data = req.body
   const giftlist = await GiftList.findByPk(id)
   await giftlist.update(data)
-  
+
   res.json({ giftlist })
 })
 
 giftListRouter.delete('/:id', async (req, res, next) => {
-  
   const id = req.params.id
   const giftlist = await GiftList.findByPk(id)
   try {
