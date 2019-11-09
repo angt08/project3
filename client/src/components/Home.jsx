2c848f13c6e2a1139182acb97ec0f878d92e9bc5
Ben Grzybowski <bengrzybowski@bens-mbp.lan>
2019-11-09 14:00:51 -0500

    added empty components and implemented home page, giftlists

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home(props) {
 
   return (
     <main className="main">
-      <h1>
-        Welcome to <b>Ben's</b> React template.
-        {
-          props.currentUser ?
-            <div>
-              <p>Hello, {props.currentUser.username}</p>
-              <button onClick={props.handleLogout}>Logout</button>
-            </div>
-            :
-            <></>
-        }
-      </h1>
+      {
+        props.currentUser ?
+          <div>
+            <p>Hello, {props.currentUser.username}</p>
+            <button onClick={props.handleLogout}>Logout</button>
+            <Link to="/giftlists/new"><button>Add an Giftlist</button></Link>
+          </div>
+          :
+          <></>
+      }
+      {props.giftLists.map(gl => (
+        <div>
+          <Link to={`/giftlists/${gl.id}`}><h3>{gl.title}</h3></Link>
+          <img src={gl.image_link} />
+          <p>{gl.description}</p>
+          <p>Due Date: {gl.due_date}</p>
+        </div>
+      ))}
     </main >
   )
 }
---
+++ client/src/components/UpdateGiftListForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UpdateGiftListForm() {
+  return (
+    <div>
+
+    </div>
+  )
+}
