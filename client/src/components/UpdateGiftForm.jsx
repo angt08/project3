ef1f52c8524311f7223e7f14fe52d77183e50586
ang <angiezafra07@gmail.com>
2019-11-12 16:03:53 -0500

    some css styling

--- client/src/components/UpdateGiftForm.jsx
+++ client/src/components/UpdateGiftForm.jsx
@@ -59,23 +59,45 @@ export default class UpdateGiftForm extends React.Component {
           this.props.handleUpdate(this.props.id, this.state)
         }} >
 
-        
+      
+          <input name="item"
+            placeholder="Gift Item"
+            onChange={this.handleChange}
+            value={this.state.formData.item} />
 
-          <input name="item" placeholder="Gift Item" onChange={this.handleChange} value={this.state.formData.item} />
+          <input name="description"
+            placeholder="Description"
+            onChange={this.handleChange}
+            value={this.state.formData.description} />
 
-          <input name="description" placeholder="Description" onChange={this.handleChange} value={this.state.formData.description} />
+          <input name="image_link"
+            placeholder="Image URL "
+            onChange={this.handleChange}
+            value={this.state.formData.image_link} />
 
-          <input name="image_link" placeholder="Image URL " onChange={this.handleChange} value={this.state.formData.image_link} />
+          <input name="price"
+            placeholder="Price"
+            onChange={this.handleChange}
+            value={this.state.formData.price} />
 
-          <input name="price" placeholder="Price" onChange={this.handleChange} value={this.state.formData.price} />
+          <input
+            name="location"
+            placeholder="Location"
+            onChange={this.handleChange}
+            value={this.state.formData.location} />
 
-          <input name="location" placeholder="Location" onChange={this.handleChange} value={this.state.formData.location} />
+          <input name="proposed_purchased_date"
+            placeholder=""
+            onChange={this.handleChange}
+            value={this.state.formData.proposed_purchased_date} />
 
-          <input name="proposed_purchased_date" placeholder="" onChange={this.handleChange} value={this.state.formData.proposed_purchased_date} />
+          <input name="actual_purchased_date"
+            placeholder="Fun Fact"
+            onChange={this.handleChange}
+            value={this.state.formData.actual_purchased_date} />
 
-          <input name="actual_purchased_date" placeholder="Fun Fact" onChange={this.handleChange} value={this.state.formData.actual_purchased_date} />
-
-          <input type="submit" value="Add Gift" />
+          <input type="submit"
+            value="Add Gift" />
 
           <Link>
             <button>Edit</button>
