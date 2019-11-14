6bc8ba6656428e104f5d4e189a2236705a703b00
Zita Zhang <zhangzita@gmail.com>
2019-11-14 09:36:20 -0500

    footer finished

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import gitHub from '../github_fpykxh.png';
-import linkedIn from '../linkedin_vnvo6s.png';
+import gitHub from '../github.png';
+// import linkedIn from '../linkedin_vnvo6s.png';
 export default function Footer(props) {
 
   return (
     <footer>
+      <p className="footerBlock">Follow Us On</p>
       <a href="https://github.com/angt08/project3">
-        <img className='image-footer' src={gitHub} border="0" alt="" />
+        {/* <img className="footerBlock" src={gitHub} border="0" alt="" /> */}
+        <div className="footerImg"></div>
       </a>
-      <p id='copyright'>Space Dinos 2019</p>
+      <p className="footerBlock">Space Dinos 2019</p>
     </footer>
   )
 }
